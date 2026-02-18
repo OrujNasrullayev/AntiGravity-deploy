3dda4532475913b10278541592457d1246586ba2
Oruj Nasrullayev <nesrullayevoruc123@gmail.com>
2026-02-19 00:51:47 +0400

    Refactor: Inlined fetching logic using closures Last

--- netlify/functions/get-notion-data.js
+++ netlify/functions/get-notion-data.js
@@ -1,4 +1,4 @@
-const Notion = require('@notionhq/client');
+const { Client } = require('@notionhq/client');
 
 
 /**
@@ -16,12 +16,11 @@ function formatId(id) {
 
 
 exports.handler = async (event, context) => {
-    // This looks at every possible place the Client could be hiding
-    const Client = Notion.Client || (Notion.default ? Notion.default.Client : Notion);
     const notion = new Client({ auth: process.env.NOTION_API_KEY });
 
-    // DEBUG LOG: Let's see what 'notion' actually looks like in the logs
-    console.log("Notion object keys:", Object.keys(notion));
+    // 3. ADD THIS SPECIFIC DEBUG LOG
+    console.log("Checking databases tool:", typeof notion.databases);
+    console.log("Checking query tool:", notion.databases ? typeof notion.databases.query : 'no databases');
 
     // Inline database fetcher
     const queryDb = async (id) => {
